99a10f24831a82c759d1d0d8deb5b91c32fa1ff9
Naveen <nooobimplementsjs@gmail.com>
2022-11-27 23:56:43 +0530

    modify livesessions.js

--- src/Component/Admins/LiveSessions.js
+++ src/Component/Admins/LiveSessions.js
@@ -3,26 +3,26 @@ import db from '../Firebase/firebase.js';
 
 function LiveSessions() {
 
-    //colllect information about 
-    useEffect(() => {
-        let tutorCollection = db.collection("Tutors");
-        //query the sessions that are on that particular day
-        let query = tutorCollection.where("Sessions", "array-contains", )
-        docRef.get().then((doc) => {
-            if (doc.exists) {
-                // console.log("Document data:", doc.data().Info);
-                const info = doc.data().Info;
-                setSessionsInfo(info.Sessions)
-                setName(info.Name);
-            } else {
-                // doc.data() will be undefined in this case
-                console.log("No such document!");
-            }
-        }).catch((error) => {
-            console.log("Error getting document:", error);
-        });
+    //colllect information about ongoing sessions
+    // useEffect(() => {
+    //     let tutorCollection = db.collection("Tutors");
+    //     //query the sessions that are on that particular day
+    //     let query = tutorCollection.where("Sessions", "array-contains", )
+    //     docRef.get().then((doc) => {
+    //         if (doc.exists) {
+    //             // console.log("Document data:", doc.data().Info);
+    //             const info = doc.data().Info;
+    //             setSessionsInfo(info.Sessions)
+    //             setName(info.Name);
+    //         } else {
+    //             // doc.data() will be undefined in this case
+    //             console.log("No such document!");
+    //         }
+    //     }).catch((error) => {
+    //         console.log("Error getting document:", error);
+    //     });
 
-    },[])
+    // },[])
 
   return (
     <div>LiveSessions</div>
