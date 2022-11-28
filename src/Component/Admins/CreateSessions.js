fa7270d2ac428d4a4aaf236c1725325901049fbf
Naveen <nooobimplementsjs@gmail.com>
2022-11-28 11:01:30 +0530

    date and time selector

--- src/Component/Admins/CreateSessions.js
+++ src/Component/Admins/CreateSessions.js
@@ -1,10 +1,22 @@
 import {React, useEffect, useState} from 'react';
 import db from '../Firebase/firebase.js';
 import MultipleSelect from './MultipleSelect.js';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import setHours from "date-fns/setHours";
+import setMinutes from "date-fns/setMinutes";
 
 function CreateSessions() {
     const [lessons, setLessons] = useState([])
-    const [students, setStudents] = useState([])
+    const [startDate, setStartDate] = useState(setHours(setMinutes(new Date(), 0), 9))
+
+    const filterPassedTime = (time) => {
+      const currentDate = new Date();
+      const selectedDate = new Date(time);
+  
+      return currentDate.getTime() < selectedDate.getTime();
+    };
+
     useEffect(()=>{
         
         db.collection("Lessons").get().then((querySnapshot) => {
@@ -25,6 +37,14 @@ function CreateSessions() {
       <div>
       <p>Welcome to create sessiosn</p>
       <MultipleSelect lessons = {lessons}/>
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        showTimeSelect
+        filterTime={filterPassedTime}
+        minDate = {new Date()}
+        dateFormat="dd/MM/yyyy, h:mm aa"
+      /> 
       </div>
     )
   }
