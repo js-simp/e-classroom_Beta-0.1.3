f8ab24d3dfa5331676bf8fb9cf1cbf4f640fb205
Naveen <nooobimplementsjs@gmail.com>
2022-11-29 10:32:52 +0530

    complete new session creation form

--- src/Component/Admins/CreateSessions.js
+++ src/Component/Admins/CreateSessions.js
@@ -1,6 +1,7 @@
 import {React, useEffect, useState} from 'react';
 import db from '../Firebase/firebase.js';
 import MultipleSelect from './MultipleSelect.js';
+import SingleSelect from './SingleSelect.js';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import setHours from "date-fns/setHours";
@@ -8,7 +9,13 @@ import setMinutes from "date-fns/setMinutes";
 
 function CreateSessions() {
     const [lessons, setLessons] = useState([])
-    const [startDate, setStartDate] = useState(setHours(setMinutes(new Date(), 0), 9))
+    const [lessonName, setLessonName] = useState([]);
+    const [startDate, setStartDate] = useState(setHours(setMinutes(new Date(), 0), 9));
+    const [sessionId, setSessionId] = useState('');
+    const [students, setStudents] = useState([]);
+    const [studentId, setStudentId] = useState('');
+    const [tutors, setTutors] = useState([]);
+    const [tutorId, setTutorId] = useState('');
     const [epoch, setEpoch] = useState()
 
     const filterPassedTime = (time) => {
@@ -17,6 +24,24 @@ function CreateSessions() {
   
       return currentDate.getTime() < selectedDate.getTime();
     };
+
+    async function handleSubmit(e) {
+      e.preventDefault();
+      let string_date = startDate.toLocaleDateString();
+      let string_time = startDate.toLocaleTimeString();
+      const sessionInfo = {
+        Date: string_date,
+        Time : string_time,
+        Status: 'Pending',
+        epochTime: epoch,
+        Lessons: lessonName,
+        studentId : studentId,
+        TutorId : tutorId
+      };
+
+      // const res = await db.collection('Sessions').doc(`${sessionId}`).set(sessionInfo);
+      console.log(sessionInfo)
+    }
 
     useEffect(()=>{
         
@@ -30,23 +55,49 @@ function CreateSessions() {
         });
         // console.log(lesson_titles)
        
+        db.collection("Students").get().then((students) => {
+          let student_list = [];
+          students.forEach((student) => {
+            student_list.push(student.id)
+          });
+          setStudents(student_list);
+        }
+        );
+
+        db.collection("Tutors").get().then((tutors) => {
+          let tutor_list = [];
+          tutors.forEach((tutor) => {
+            tutor_list.push(tutor.id)
+          });
+          setTutors(tutor_list);
+        }
+        );
         
     },[])
     
-  if(lessons.length != 0) {
+  if(lessons.length != 0 && students.length != 0) {
     return (
-      <div>
-      <p>Welcome to create sessiosn</p>
-      <MultipleSelect lessons = {lessons}/>
-      <DatePicker
-        selected={startDate}
-        onChange={(date) => {setStartDate(date); setEpoch(Math.floor(date.getTime()/ 1000))}}
-        showTimeSelect
-        filterTime={filterPassedTime}
-        minDate = {new Date()}
-        dateFormat="MM/dd/yyyy HH:mm"
-      /> 
-      </div>
+      <form onSubmit={handleSubmit}>
+        <MultipleSelect  lessons = {lessons} selectLesson = {setLessonName}/>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => {setStartDate(date); setEpoch(Math.floor(date.getTime()/ 1000))}}
+          showTimeSelect
+          filterTime={filterPassedTime}
+          minDate = {new Date()}
+          dateFormat="MM/dd/yyyy HH:mm"
+        />
+        <label>SessionId:
+        <input
+          type="text" 
+          value={sessionId}
+          onChange={(e) => setSessionId(e.target.value)}
+        />
+      </label>
+        <SingleSelect list = {students} selectItem = {setStudentId} label = 'Student ID'/>
+        <SingleSelect list = {tutors} selectItem = {setTutorId} label = 'Tutor ID'/>
+        <input type="submit" value="SUBMIT"/>
+      </form>
     )
   }
   else{
--- src/Component/Admins/MultipleSelect.js
+++ src/Component/Admins/MultipleSelect.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import {React, useRef, useState} from 'react';
 import {useTheme} from '@material-ui/core/styles';
-import { Select, FormControl, InputLabel, MenuItem, OutlinedInput } from '@material-ui/core';
+import { Select,Box, Chip, FormControl, InputLabel, MenuItem, OutlinedInput } from '@material-ui/core';
 
   const ITEM_HEIGHT = 48;
   const ITEM_PADDING_TOP = 8;
@@ -16,29 +16,38 @@ import { Select, FormControl, InputLabel, MenuItem, OutlinedInput } from '@mater
 function MultipleSelect(props) {
     
     const lessons = props.lessons;
-    const [lessonName, setLessonName] = React.useState([]);
-    
+    const [selection, setSelection] = useState([])
+
       const handleChange = (event) => {
         const {
           target: { value },
         } = event;
-        setLessonName(
+        props.selectLesson(
           // On autofill we get a stringified value.
+          typeof value === 'string' ? value.split(',') : value,
+        );
+        setSelection(
           typeof value === 'string' ? value.split(',') : value,
         );
       };
     
       return (
-        <div>
           <FormControl sx={{ m: 1, width: 300 }}>
             <InputLabel id="demo-multiple-name-label">Lessons</InputLabel>
             <Select
               labelId="lesson-multiple-select"
               id="lesson-titles"
               multiple
-              value={lessonName}
+              value={selection}
               onChange={handleChange}
               input={<OutlinedInput label="LessonName" />}
+              renderValue={(selected) => (
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                  {selected.map((value) => (
+                    <Chip key={value} label={value} />
+                  ))}
+                </Box>
+              )}
               MenuProps={MenuProps}
             >
               {lessons.map((lesson) => (
@@ -51,7 +60,6 @@ function MultipleSelect(props) {
               ))}
             </Select>
           </FormControl>
-        </div>
       );
 }
 
